c300abd15c62d973643bc5a60997039368b648df
KwameSei <nathanielosei92@gmail.com>
2023-12-19 05:21:14 +0000

    Project structure and basic server setup

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const app = express();
+
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Welcome to the Test API Platform'
+  });
+});
+
+
+export default app;
