ca6f6ae9c7cbe70353699f8583f39c75cb2d52df
KwameSei <nathanielosei92@gmail.com>
2023-12-22 15:07:42 +0000

    middleware folder moved from controllers folder to main directory

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authentication, authorization } from '../controllers/middleware/auth.js';
+import { authentication, authorization } from '../middleware/auth.js';
 import userController from '../controllers/userController.js';
 
 const router = express.Router();
