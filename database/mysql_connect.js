baeba1170a96a5f49e704dbf26dcbbf259c59818
KwameSei <nathanielosei92@gmail.com>
2023-12-24 05:37:35 +0000

    test database migration and seeding completed

--- database/mysql_connect.js
+++ database/mysql_connect.js
@@ -6,6 +6,46 @@ import knex from 'knex';
 
 dotenv.config();
 
+let config = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    multipleStatements: true,
+  },
+}
+
+// configuration for test database connection
+if (process.env.NODE_ENV === 'test') {
+  config = {
+    client: 'mysql2',
+    connection: {
+      host: process.env.TEST_DB_HOST,
+      port: process.env.TEST_DB_PORT,
+      user: process.env.TEST_DB_USER,
+      password: process.env.TEST_DB_PASSWORD,
+      database: process.env.TEST_DB_DATABASE,
+      multipleStatements: true,
+    },
+  };
+}
+
+// Pass the config to knex to establish the connection.
+const knexConnection = knex(config);
+
+export default knexConnection;
+
+// NB: To run the server, use either of the command below:
+// NODE_ENV=development yarn dev
+// NODE_ENV=test yarn test
+
+
+
+
+
 // const poolConnect = createPool({
 //   host: process.env.DB_HOST,
 //   port: process.env.DB_PORT,
@@ -25,56 +65,3 @@ dotenv.config();
 // });
 
 // export default poolConnect;
-
-// export const dbConnection = createConnection({
-//   host: process.env.HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DB,
-// });
-
-// dbConnection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL database:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database!');
-// });
-
-// export default dbConnection;
-
-
-// const connectDB = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'your_username',
-//   password: 'your_password',
-//   database: 'your_database_name'
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL database:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database!');
-// });
-
-// export default connectDB;
-
-let config = {
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    multipleStatements: true,
-  },
-}
-
-// Pass the config to knex to establish the connection.
-const knexConnection = knex(config);
-
-export default knexConnection;
--- server.js
+++ server.js
@@ -6,3 +6,8 @@ dotenv.config();
 app.listen(process.env.APP_PORT, () => {
   console.log('Server is running on port: ', process.env.APP_PORT);
 })
+
+
+// NB: To run the server, use either of the command below:
+// NODE_ENV=development yarn dev
+// NODE_ENV=test yarn test
